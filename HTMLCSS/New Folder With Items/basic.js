8e19d2d16514f25b7f9f207a7d33a12da2a93d3e
Digvijay Singh <dvsingh958@gmail.com>
2023-04-06 00:04:58 +0530

    html css files added

---
+++ HTMLCSS/New Folder With Items/basic.js
@@ -0,0 +1,48 @@
+// // var name= "Digvijay";
+
+// // var rollNumber= "123456";
+// // var active = true;
+// // var college = 'Newton School';
+// // var branch = "CSE";
+
+// // console.log("This is line 1");
+// // console.log("This is line 2");
+// // console.log("10+20 = 30");
+// // console.log("Checking symbols!@#$%$%^^");
+// // console.log("This is the last line");
+
+// // var abc = "This is first variable";
+// // var bcd = "123456";
+// // var efg = "23";
+// // var ghi = "27";
+// // var jkl = true;
+
+// // debugger
+
+// // console.log(abc);
+// // console.log(bcd);
+// // console.log(efg);
+// // console.log(ghi);
+// // console.log(jkl);
+
+// // console.log("What is the first variable:",abc,"and the number is",bcd);
+
+// // console.log("Data type of abc:", typeof(abc), "Data type of bcd:", typeof(bcd), "Data type of jkl:", typeof(jkl));
+
+// // var num1 = 20;
+// // var num2 = 30;
+// // var num3 = num1 - num2;
+// // console.log((num1+num2), typeof(num3), "the variable 3 is", num3);
+
+// // console.log(String);
+// // console.log(typeof output);
+
+// var p = '1234';
+// var pp = '1234';
+
+// var num = Number(p);
+// var num1 = Number('pp');
+
+// console.log(num, num1);
+
+console.log("Hello World")
