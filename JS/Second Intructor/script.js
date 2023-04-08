e0205ff84f4730da3817333687010329b20c10e7
Digvijay Singh <dvsingh958@gmail.com>
2023-04-08 15:10:56 +0530

    second insructor class js file added

---
+++ JS/Second Intructor/script.js
@@ -0,0 +1,16 @@
+let a
+var b
+console.log("working")
+let c
+var d
+const e = 15;
+
+f = 20;
+var f = 30
+console.log(f)
+
+const array = [1, 2, [1,2,3,4], 'abc']
+
+for(item in array){
+    console.log()
+}
