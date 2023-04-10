351ee265f12edf7fdd1011fcb684e9690ae5e295
Digvijay Singh <dvsingh958@gmail.com>
2023-04-10 23:37:34 +0530

    second instructor class 10 Apr

--- JS/Second Intructor/script.js
+++ JS/Second Intructor/script.js
@@ -1,16 +1,61 @@
-let a
-var b
-console.log("working")
-let c
-var d
-const e = 15;
+// let a
+// var b
+// console.log("working")
+// let c
+// var d
+// const e = 15;
 
-f = 20;
-var f = 30
-console.log(f)
+// f = 20;
+// var f = 30
+// console.log(f)
 
-const array = [1, 2, [1,2,3,4], 'abc']
+// const array = [1, 2, [1,2,3,4], 'abc']
 
-for(item in array){
-    console.log()
+// for(item in array){
+//     console.log()
+// }
+
+// function addition(arr){
+//     let sum = 0;
+//     for(let value of arr){
+//         let num = value
+//         if(typeof num == 'string'){
+//             num = +num
+//         }
+//         if(typeof num == 'number'){
+//             sum = sum + num;
+//         }
+//         // if(typeof value == 'number'){
+//         //     sum = sum + value
+//         // }
+//     }
+//     console.log(sum)
+// }
+
+// let arr = [1,2,3,4,5, '6', '7', undefined, true, false, 3, '6']
+// addition(arr)
+
+// addition(2,4)
+// functionName(2,4)
+// let a = 12
+// let b = true
+// addition(a,b)
+// functionName(a,b)
+
+// function functionName(a,b){
+//     console.log("the value of a is ", a, "the value of b is ", b)
+// }
+
+function addNumber(arr){
+    let sum = 0;
+    for(let value of arr){
+        if(isNaN(value) || typeof value == 'boolean'){
+            continue;
+        }
+        sum = sum + (+value)
+    }
+    console.log(sum)
 }
+
+const arr = [1, 2, 3, 4, 5, null, undefined, "", '1']
+addNumber(arr)
