11e7fc683038f7b3fbc756740c1a25af43a1d740
Digvijay Singh <dvsingh958@gmail.com>
2023-04-15 12:57:39 +0530

    second instructor 14 April

--- JS/Second Intructor/script.js
+++ JS/Second Intructor/script.js
@@ -46,16 +46,40 @@
 //     console.log("the value of a is ", a, "the value of b is ", b)
 // }
 
-function addNumber(arr){
-    let sum = 0;
-    for(let value of arr){
-        if(isNaN(value) || typeof value == 'boolean'){
-            continue;
-        }
-        sum = sum + (+value)
-    }
-    console.log(sum)
+// function addNumber(arr){
+//     let sum = 0;
+//     for(let value of arr){
+//         if(isNaN(value) || typeof value == 'boolean'){
+//             continue;
+//         }
+//         sum = sum + (+value)
+//     }
+//     console.log(sum)
+// }
+
+// const arr = [1, 2, 3, 4, 5, null, undefined, "", '1']
+// addNumber(arr)
+
+// console.log('function' ,callme);
+
+// function callme (a) {
+// var b =30;
+// return b*a;
+// }
+
+// var c = callme(3);
+// console.log(callme(5))
+
+//console.log(call)
+
+var scopeFinal = "final";
+
+{
+    var scopeFinal = "final1";
+    let block = "Scope";
+    console.log(block) //
+    console.log(scopeFinal) //
 }
 
-const arr = [1, 2, 3, 4, 5, null, undefined, "", '1']
-addNumber(arr)
+console.log(scopeFinal) //
+console.log(block); //
