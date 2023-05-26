9fa77e52a37f28f62bdbb7b57f98743c4b723f81
Digvijay Singh <dvsingh958@gmail.com>
2023-05-26 18:28:06 +0530

    promise assignment

--- JS/First Instructor/Lectures/May 25/script.js
+++ JS/First Instructor/Lectures/May 25/script.js
@@ -1,17 +1,31 @@
 
-async function kanyeQuote(){
-    const response = await fetch('https://api.kanye.rest/');
-    console.log(response);
-    const promise = await response.json();
-    console.log(promise);
+// async function kanyeQuote(){
+//     const response = await fetch('https://api.kanye.rest/');
+//     console.log(response);
+//     const promise = await response.json();
+//     console.log(promise);
 
-    const{quote} = promise;
-    console.log(quote);
-}
+//     const{quote} = promise;
+//     console.log(quote);
+// }
 
-function addToDom(){
-    const elem = document.createElement("div");
+// function addToDom(){
+//     const elem = document.createElement("div");
     
+// }
+
+// kanyeQuote();
+
+function promiseMe(time, data) {
+    const promise1 = new Promise((data)=>{
+        setTimeout(()=>{
+            return data;
+        }, time);
+    });
+
+    return promise1;
 }
 
-kanyeQuote();
+promiseMe(200, 'hi').then(data=>{
+    console.log(data) // prints hi
+})
