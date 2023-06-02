06b67d21048ab0fbdd039803e93d82b1baa0b144
Digvijay Singh <dvsingh958@gmail.com>
2023-06-02 14:08:44 +0530

    github issues assignment completed

--- JS/Assignment/Github issues/script.js
+++ JS/Assignment/Github issues/script.js
@@ -1,25 +1,46 @@
 const nextBtn = document.getElementById("load_next");
 const prevBtn = document.getElementById("load_prev");
+const pageDisplay = document.getElementById("page_indicator");
+const issueDisplay = document.getElementById("issue_container");
 let pageNum = 1;
 
-nextBtn.addEventListener("click", async () => {
-  if (pageNum >= 1 && pageNum < 5) {
-    const data = await fetch(
-      `https://api.github.com/repositories/1296269/issues?page=${pageNum}&per_page=5`
-    );
-    pageNum++;
-    const data1 = await data.json();
-    console.log(data1);
+async function displayIssues() {
+  const data = await fetch(
+    `https://api.github.com/repositories/1296269/issues?page=${pageNum}&per_page=5`
+  );
+  const data1 = await data.json();
+  issueDisplay.innerHTML = "";
+  data1.forEach((issue) => {
+    issueName = issue.title;
+    const listElem = document.createElement("li");
+    listElem.textContent = issueName;
+    issueDisplay.insertAdjacentElement("beforeend", listElem);
+  });
+
+  pageDisplay.innerText = pageNum;
+  if (pageNum === 1) {
+    prevBtn.disabled = true;
+  } else {
+    prevBtn.disabled = false;
   }
+
+  if (pageNum < 5) {
+    nextBtn.disabled = false;
+  } else {
+    nextBtn.disabled = true;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  displayIssues();
 });
 
-prevBtn.addEventListener("click", async () => {
-  if (pageNum > 1 && pageNum <= 5) {
-    pageNum--;
-    const data = await fetch(
-      `https://api.github.com/repositories/1296269/issues?page=${pageNum}&per_page=5`
-    );
-    const data1 = await data.json();
-    console.log(data1);
-  }
+nextBtn.addEventListener("click", () => {
+  pageNum++;
+  displayIssues();
+});
+
+prevBtn.addEventListener("click", () => {
+  pageNum--;
+  displayIssues();
 });
