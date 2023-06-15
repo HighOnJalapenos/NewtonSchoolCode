05ea5754e64ef42446e9a5ad0fa3611e332b38ef
Digvijay Singh <dvsingh958@gmail.com>
2023-06-15 18:47:28 +0530

    minesweeper assignment incomplete

--- JS/Assignment/Minesweeper/script.js
+++
@@ -1,8 +0,0 @@
-const gridContainer = document.getElementById("gridContainer");
-
-for (let i = 1; i <= 81; i++) {
-  const div = document.createElement("div");
-  div.id = `cell_${i}`;
-  div.style.backgroundColor = "white";
-  gridContainer.appendChild(div);
-}
