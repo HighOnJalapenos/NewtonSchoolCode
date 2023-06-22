0b52d3f0227c629bfc7060760ef35bc70784785e
Digvijay Singh <dvsingh958@gmail.com>
2023-06-22 13:58:19 +0530

    array merger contest question completed

--- JS/Assignment/Js Array Merger/script.js
+++ JS/Assignment/Js Array Merger/script.js
@@ -1,47 +1,52 @@
-
-const arr1 = document.getElementById('1');
-const arr2 = document.getElementById('2');
-const btn1 = document.getElementById('add-1');
-const btn2 = document.getElementById('add-2');
+const arr1 = document.getElementById("1");
+const arr2 = document.getElementById("2");
+const btn1 = document.getElementById("add-1");
+const btn2 = document.getElementById("add-2");
+const result = document.getElementById("result");
+const error = document.getElementById("error");
 
 const firstArray = [];
 const secondArray = [];
 
-btn1.addEventListener('click', () => {
-    firstArray.length = 0;
-    let flag = true;
-    let inputArray = arr1.value.split(",");
-    let outputDiv = document.getElementById("disp-1");
-    inputArray.forEach(element => {
-        if(isNaN(element)){
-            outputDiv.textContent = "Wrong Input!";
-            flag = false;
-            break;
-        } else{
-            firstArray.push(+element);
-        }
-    });
+btn1.addEventListener("click", () => {
+  firstArray.length = 0;
+  let flag = true;
+  let inputArray = arr1.value.split(",");
+  let outputDiv = document.getElementById("disp-1");
+  outputDiv.textContent = "";
+  flag = inputArray.every((element) => {
+    return !isNaN(element);
+  });
 
-    if(flag) outputDiv.textContent = `[${firstArray}]`;
-})
+  if (flag) {
+    firstArray.push(...inputArray);
+    outputDiv.textContent = `[${firstArray}]`;
+    error.classList.add("d-none");
+  } else {
+    error.classList.remove("d-none");
+  }
+});
 
-btn2.addEventListener('click', () => {
-    firstArray.length = 0;
-    let flag = true;
-    let inputArray = arr2.value.split(",");
-    let outputDiv = document.getElementById("disp-2");
-    inputArray.forEach(element => {
-        if(isNaN(element)){
-            outputDiv.textContent = "Wrong Input!";
-            flag = false;
-            break;
-        } else{
-            secondArray.push(+element);
-        }
-    });
-    
-    if(flag) outputDiv.textContent = `[${secondArray}]`;
-})
+btn2.addEventListener("click", () => {
+  secondArray.length = 0;
+  let flag = true;
+  let inputArray = arr2.value.split(",");
+  let outputDiv = document.getElementById("disp-2");
+  outputDiv.textContent = "";
+  flag = inputArray.every((element) => {
+    return !isNaN(element);
+  });
 
-document.getElementById('merge').addEventListener('click', () => {
-})
+  if (flag) {
+    secondArray.push(...inputArray);
+    outputDiv.textContent = `[${secondArray}]`;
+    error.classList.add("d-none");
+  } else {
+    error.classList.remove("d-none");
+  }
+});
+
+document.getElementById("merge").addEventListener("click", () => {
+  const mergedArray = [...firstArray, ...secondArray];
+  result.textContent = `[${mergedArray}]`;
+});
